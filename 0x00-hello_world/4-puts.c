57b4a8c6fa1c60b47aca6dbed6339ff38fede746
DAVIESROTA <daviesrota@gmail.com>
2023-02-16 14:18:06 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#Include <stdio.h>
+#include <stdio.h>
 /**
  * main - entry
  * Return: Always 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
 
