95c658df8f296b313cdd735016318a23d64cfdb2
DAVIESROTA <daviesrota@gmail.com>
2023-02-27 12:37:08 -0800

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
 *  reset_to_98 - takes the pointer and updates the value
-22*  Return: 0 is success
+*  Return: 0 is success
 */
 
 void reset_to_98(int *n)
 {
-	int n;
-	*n = 98;
+		*n = 98;
 }
