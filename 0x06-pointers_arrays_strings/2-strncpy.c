ff1450c26e5d0be3ceca85457774c8f0eb5150f2
DAVIESROTA <daviesrota@gmail.com>
2023-03-04 04:29:25 -0800

    strcpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+char *_strncpy(char *dest, char *src, int n);
+int i = 0;
+int j = 0;
+
+for(i =0 i; i < n && *(src + 1); i++);
+{
+	*(dest + i) = *(src + i);
+}
+for(i < n; i++);
+{
+	*(dest + i) = '\n';
+	return dest;
+}
+	
+
+
