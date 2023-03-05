5fb655a8dff4b22abcacc5bc1b0b3cdcbdc11385
DAVIESROTA <daviesrota@gmail.com>
2023-03-05 02:50:59 -0800

    lowercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+
+char *string_toupper(char *str) {
+    char *p = str;
+    while (*p != '\0') {
+        if (islower(*p)) {
+            *p = toupper(*p);
+        }
+        p++;
+    }
+    return str;
+}
+
