4da8b3fb3bc254c96b6ec4f569e4c355a1fd73ba
DAVIESROTA <daviesrota@gmail.com>
2023-03-21 13:57:15 -0700

    morepointers and free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,19 @@
-#include "main.h""
+#include "main.h"
 
+/**
+ * malloc_checked - cause normal process termination with a status value of 98
+ *
+ * @b: allocated memory
+ *
+ * Return: a pointer to the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
-	void i;
+	void *i;
 
 	i = malloc(b);
-	if(i == NULL)
-		exit 98;
+	if (i == NULL)
+		exit(98);
 	return (i);
+
 }
