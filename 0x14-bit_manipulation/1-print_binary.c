fa3c934acc8dbbb90c83a6d2d406f4a928f900ac
DAVIESROTA <daviesrota@gmail.com>
2023-04-17 16:18:56 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,17 +7,18 @@
  * *Return: no return
  */
 
-void print_binary(unsigned long integer n)
+void print_binary(unsigned long int n)
 {
 	if (n >> 0)
 	{
 		if (n >> 1)
 			print_binary(n >> 1);
 		_putchar((n & 1) + '0');
-		else
+	}
+	else
 		{
 			_putchar(0);
 		}
-	}
 }
+
 
