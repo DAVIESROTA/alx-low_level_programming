a15663b241cbdb6cf08cd2cec39753aefb879cbe
DAVIESROTA <daviesrota@gmail.com>
2023-04-17 19:33:02 -0700

    task o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
-#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f;
-	ssize_t lnr, lnw;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	f = open (filename, O_RDONLY);
-	if (f == -1)
-		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer = NULL)
-	{
-		close(f);
+	if (buffer == NULL)
 		return (0);
-	}
-	lnr = read(f, buffer, letters);
-	close (f);
-	if (lnr == -1)
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
-	lnw = write(STDOUT_FILENO, buffer, lnr);
+
 	free(buffer);
-	if (lnr != lnw)
-		return (0);
-	return (lnw);
+	close(o);
+
+	return (w);
 }
