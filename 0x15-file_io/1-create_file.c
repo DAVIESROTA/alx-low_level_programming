ec384333a41cffaea320341ef664254022baa500
DAVIESROTA <daviesrota@gmail.com>
2023-04-17 19:52:01 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+	int lettern;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (f == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (lettern = 0; text_content[lettern]; lettern++);
+
+	rwr = write(f, text_content, lettern);
+	 if (rwr == -1)
+		 return (-1);
+	 close f;
+	 return (1);
+}
+
+
