a07a7e1ef8640c5f456e1e694630ec7a14385b44
DAVIESROTA <daviesrota@gmail.com>
2023-04-17 19:55:55 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,9 @@ int create_file(const char *filename, char *text_content)
 	rwr = write(f, text_content, lettern);
 	 if (rwr == -1)
 		 return (-1);
-	 close f;
+
+	 close; f;
+
 	 return (1);
 }
 
